bca47f1b9550f01801f09b25273b19ee92cbb264
Ryan Cheng <zrc2106@tc.columbai.edu>
2018-12-20 16:25:16 -0500

    remove the folder

---
+++ main.js
@@ -0,0 +1,15 @@
+var x = document.getElementById("myProfile");
+
+var myHeight = x.style.height;
+
+
+function myFunction() {
+
+    if (x.style.height == "10px") {
+        x.style.height = "120px";
+    } else {
+        x.style.height = "10px";
+    }
+}
+
+//test
